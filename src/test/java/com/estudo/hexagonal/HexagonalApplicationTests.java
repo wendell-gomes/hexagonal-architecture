8fe9c4ff58c60dbddf4019c90ccad265c4286b94
Wendell <w3ndell@hotmail.com>
2024-05-01 15:32:55 -0300

    Commit inicial

---
+++ src/test/java/com/estudo/hexagonal/HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
